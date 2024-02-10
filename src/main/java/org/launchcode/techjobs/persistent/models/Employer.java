145b56aef95deceff756de0ae3c79249ef2382b2
carnasci <cen.master24@gmail.com>
2024-02-10 13:23:54 -0600

    on test-task 2 end

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank
+    @Size(max = 255, message = "Location can only be 255 characters long.")
+    private String location;
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
